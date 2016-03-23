4c94beda9fd3a383141aee9bcf956006777973c3
paranoialove <akazuma82@gmail.com>
2016-03-23 19:02:54 +0800

    更新檔案

--- Dump20160323.sql
+++ Dump20160323.sql
@@ -70,7 +70,7 @@ CREATE TABLE `burger_flaver` (
 
 LOCK TABLES `burger_flaver` WRITE;
 /*!40000 ALTER TABLE `burger_flaver` DISABLE KEYS */;
-INSERT INTO `burger_flaver` VALUES (1,'燒烤牛肉 / ROAST BEEF',110,180,NULL,'受歡迎的SUBWAY®經典美味-低脂、柔嫩的香烤牛肉切片配上自選新鮮蔬菜和醬料，夾入新鮮出爐的麵包裡，簡約而不簡單。',0,'牛肉'),(2,'百味俱樂部 / SUBWAY CLUB',110,180,NULL,'百味俱樂部(Subway Club) 是另一款美味潛艇堡。真正的百味融合，柔嫩的精選雞胸肉，煙燻的香烤牛肉，美味的火腿切片，現場烤製的麵包，配上自選的蔬菜和醬料，獨特的味覺挑逗，捨我其誰！',0,'牛肉'),(3,'火雞胸肉 / TURKEY BREAST',95,155,NULL,'何必等到感恩節？現在就來享受美味的火雞胸肉(Turkey Breast)潛艇堡吧。柔嫩火雞胸肉切片，保證低脂，風味獨特，麵包現場烤製，配上自選的新鮮蔬菜和醬料，可口的潛艇堡就此誕生!',0,'火雞'),(4,'火雞火腿 /TURKEY BREAST & HAM',95,155,NULL,'這款內容豐富的潛艇堡，入口齒頰留香，您根本沒意識自己正在享用低脂食品。在新鮮出爐的麵包裡夾入一層美味火雞火腿切片，鋪上自選的新鮮蔬菜，淋上不含脂肪的醬料。',0,'火雞'),(5,'嫩切雞肉 / SLICED CHICKEN',NULL,999,NULL,'鮮嫩雞肉切片，美味低脂，搭配自選新鮮蔬菜及醬料，豐富的口感讓人難以抗拒。',0,'雞肉'),(6,'香烤雞肉 / ROASTED CHICKEN',95,155,NULL,'是不是想吃雞肉了？那麼，就嚐一嘗SUBWAY®的香烤雞肉吧。柔嫩無骨的雞肉烤得恰到好處，配上自選的新鮮蔬菜和獨特醬料，麵包新鮮出爐，鮮香四溢，那個味道，嘗起來是否如媽媽親手做的？',0,'雞肉'),(7,'照燒雞肉 / CHICKEN TERIYAKI',NULL,999,NULL,'這是一款風靡亞洲的經典美食，在柔嫩的雞肉切片上淋上少量照燒調味醬，搭配自選的新鮮蔬菜和調味品，一起裹入新鮮出爐的麵包。',0,'雞肉'),(8,'火腿 / HAM',85,145,NULL,'配上鮮嫩多汁的片片火腿，美味難以抵擋，更何況脂肪只有少於6克。',0,NULL),(9,'素食蔬菜 / VEGGIE DELITE™',65,105,NULL,'現場烤製的麵包和各種蔬菜匯聚一堂：生菜、蕃茄，青椒、洋蔥、橄欖和酸黃瓜，配上爽口開胃的醬料，看上去像個沙拉潜艇堡，美味讓您大吃一驚!',0,'素食'),(10,'雞肉丸 / CHICKEN MEATBALL',75,125,NULL,'雞肉丸沉浸在我們獨有義大利番茄醬（這是SUBWAY® 創始人媽媽的獨家秘方）裡，配以自選的新鮮蔬菜和醬料，最後夾入新鮮出爐的麵包。請盡情享用吧!',0,'雞肉'),(11,'火雞臘腸 / COLD CUT TRIO',75,125,NULL,'SUBWAY®潛艇堡系列中的傳統款，讓您品嘗到片片美味的義式煙燻臘腸、火雞火腿和義式臘味香腸，搭配自選蔬菜和醬料，並與每日新鮮烘製麵包一同上桌。',0,'火雞'),(12,'鮪魚 / TUNA',85,145,NULL,'鮪魚與新鮮美乃滋的混合，創造出世上最妙不可言的滋味。 SUBWAY® 鮪魚(Tuna) 潛艇堡可自選蔬菜、醬料以及新鮮出爐的各式麵包 。',0,'海鮮'),(13,'義大利經典 / ITALIAN B.M.T',110,180,NULL,'經典系列中備受青睞的美味。在新鮮出爐的麵包裡夾入義式煙燻臘腸、義大利辣香腸和火腿切片，佐以自選的新鮮蔬菜，最後淋上醬料。',0,NULL),(14,'香熱起司 / SUBWAY MELT',110,180,NULL,'柔嫩的火雞胸肉，火腿，滋滋作響的培根，濃濃的芝士，配以清脆的蔬菜和獨特醬料，一切都融合在新鲜出爐的麵包内， 這種美妙滋味只能意會不可言傳。立刻動身前往最近的SUBWAY® 一飽口福吧。',0,'火雞'),(15,'起司牛排 / STEAK & CHEESE',110,180,NULL,'靈感來自於費城著名的三明治(Hoagie)，而SUBWAY®的起司牛排(Cheese Steak) 嘗來更是不惶多讓，令人垂涎三尺的低脂、柔嫩的牛排切片，佐以嫩炒洋蔥和青椒，覆上一層熱融融的芝士，再配上各種自選的新鮮蔬菜和醬料，夾在新鮮出爐的麵包裡，熱氣騰騰，令人食指大動。',0,'牛肉');
+INSERT INTO `burger_flaver` VALUES (1,'燒烤牛肉 / ROAST BEEF',110,180,'img/burger/flaver/RoastBeef.jpg','受歡迎的SUBWAY®經典美味-低脂、柔嫩的香烤牛肉切片配上自選新鮮蔬菜和醬料，夾入新鮮出爐的麵包裡，簡約而不簡單。',0,'牛肉'),(2,'百味俱樂部 / SUBWAY CLUB',110,180,'img/burger/flaver/SubwayClub.jpg','百味俱樂部(Subway Club) 是另一款美味潛艇堡。真正的百味融合，柔嫩的精選雞胸肉，煙燻的香烤牛肉，美味的火腿切片，現場烤製的麵包，配上自選的蔬菜和醬料，獨特的味覺挑逗，捨我其誰！',0,'牛肉'),(3,'火雞胸肉 / TURKEY BREAST',95,155,'img/burger/flaver/TurkeyBreast.jpg','何必等到感恩節？現在就來享受美味的火雞胸肉(Turkey Breast)潛艇堡吧。柔嫩火雞胸肉切片，保證低脂，風味獨特，麵包現場烤製，配上自選的新鮮蔬菜和醬料，可口的潛艇堡就此誕生!',0,'火雞'),(4,'火雞火腿 /TURKEY BREAST & HAM',95,155,'img/burger/flaver/TurkeyBreastHam.jpg','這款內容豐富的潛艇堡，入口齒頰留香，您根本沒意識自己正在享用低脂食品。在新鮮出爐的麵包裡夾入一層美味火雞火腿切片，鋪上自選的新鮮蔬菜，淋上不含脂肪的醬料。',0,'火雞'),(5,'嫩切雞肉 / SLICED CHICKEN',95,155,'img/burger/flaver/SlicedChicken.jpg','鮮嫩雞肉切片，美味低脂，搭配自選新鮮蔬菜及醬料，豐富的口感讓人難以抗拒。',0,'雞肉'),(6,'香烤雞肉 / ROASTED CHICKEN',95,155,'img/burger/flaver/RoadstedChicken.jpg','是不是想吃雞肉了？那麼，就嚐一嘗SUBWAY®的香烤雞肉吧。柔嫩無骨的雞肉烤得恰到好處，配上自選的新鮮蔬菜和獨特醬料，麵包新鮮出爐，鮮香四溢，那個味道，嘗起來是否如媽媽親手做的？',0,'雞肉'),(7,'照燒雞肉 / CHICKEN TERIYAKI',95,155,'img/burger/flaver/ChickenTeriyaki.jpg','這是一款風靡亞洲的經典美食，在柔嫩的雞肉切片上淋上少量照燒調味醬，搭配自選的新鮮蔬菜和調味品，一起裹入新鮮出爐的麵包。',0,'雞肉'),(8,'火腿 / HAM',85,145,'img/burger/flaver/Ham.jpg','配上鮮嫩多汁的片片火腿，美味難以抵擋，更何況脂肪只有少於6克。',0,NULL),(9,'素食蔬菜 / VEGGIE DELITE',65,105,'img/burger/flaver/VeggieDelite.jpg','現場烤製的麵包和各種蔬菜匯聚一堂：生菜、蕃茄，青椒、洋蔥、橄欖和酸黃瓜，配上爽口開胃的醬料，看上去像個沙拉潜艇堡，美味讓您大吃一驚!',0,'素食'),(10,'雞肉丸 / CHICKEN MEATBALL',75,125,'img/burger/flaver/ChickenMeatball.jpg','雞肉丸沉浸在我們獨有義大利番茄醬（這是SUBWAY® 創始人媽媽的獨家秘方）裡，配以自選的新鮮蔬菜和醬料，最後夾入新鮮出爐的麵包。請盡情享用吧!',0,'雞肉'),(11,'火雞臘腸 / COLD CUT TRIO',75,125,'img/burger/flaver/ColdCutTrio.jpg','系列中的傳統款，讓您品嘗到片片美味的義式煙燻臘腸、火雞火腿和義式臘味香腸，搭配自選蔬菜和醬料，並與每日新鮮烘製麵包一同上桌。',0,'火雞'),(12,'鮪魚 / TUNA',85,145,'img/burger/flaver/Tuna.jpg','鮪魚與新鮮美乃滋的混合，創造出世上最妙不可言的滋味。 SUBWAY® 鮪魚(Tuna) 潛艇堡可自選蔬菜、醬料以及新鮮出爐的各式麵包 。',0,'海鮮'),(13,'義大利經典 / ITALIAN B.M.T',110,180,'img/burger/flaver/ItalianBMT.jpg','經典系列中備受青睞的美味。在新鮮出爐的麵包裡夾入義式煙燻臘腸、義大利辣香腸和火腿切片，佐以自選的新鮮蔬菜，最後淋上醬料。',0,NULL),(14,'香熱起司 / SUBWAY MELT',110,180,'img/burger/flaver/SubwayMELT.jpg','柔嫩的火雞胸肉，火腿，滋滋作響的培根，濃濃的芝士，配以清脆的蔬菜和獨特醬料，一切都融合在新鲜出爐的麵包内， 這種美妙滋味只能意會不可言傳。立刻動身前往最近的SUBWAY® 一飽口福吧。',0,'火雞'),(15,'起司牛排 / STEAK & CHEESE',110,180,'img/burger/flaver/SteakCheese.jpg','靈感來自於費城著名的三明治(Hoagie)，而SUBWAY®的起司牛排(Cheese Steak) 嘗來更是不惶多讓，令人垂涎三尺的低脂、柔嫩的牛排切片，佐以嫩炒洋蔥和青椒，覆上一層熱融融的芝士，再配上各種自選的新鮮蔬菜和醬料，夾在新鮮出爐的麵包裡，熱氣騰騰，令人食指大動。',0,'牛肉');
 /*!40000 ALTER TABLE `burger_flaver` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,18 +137,31 @@ DROP TABLE IF EXISTS `burgertemp`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `burgertemp` (
   `cid` char(10) NOT NULL,
-  `date` date DEFAULT NULL,
+  `adddate` date DEFAULT NULL,
   `breadId` int(11) DEFAULT NULL,
   `breadName` varchar(45) DEFAULT NULL,
   `flaverId` int(11) DEFAULT NULL,
   `flaverName` varchar(45) DEFAULT NULL,
   `flaverPrice` int(11) DEFAULT NULL,
-  `sauseId` int(11) DEFAULT NULL,
-  `sauseName` varchar(45) DEFAULT NULL,
-  `vegId` int(11) DEFAULT NULL,
-  `vegName` varchar(45) DEFAULT NULL,
+  `selectedSause01` tinyint(1) DEFAULT '0',
+  `selectedSause02` tinyint(1) DEFAULT '0',
+  `selectedSause03` tinyint(1) DEFAULT '0',
+  `selectedSause04` tinyint(1) DEFAULT '0',
+  `selectedSause05` tinyint(1) DEFAULT '0',
+  `selectedSause06` tinyint(1) DEFAULT '0',
+  `selectedSause07` tinyint(1) DEFAULT '0',
+  `selectedSause08` tinyint(1) DEFAULT '0',
+  `selectedVeg01` tinyint(1) DEFAULT '0',
+  `selectedVeg02` tinyint(1) DEFAULT '0',
+  `selectedVeg03` tinyint(1) DEFAULT '0',
+  `selectedVeg04` tinyint(1) DEFAULT '0',
+  `selectedVeg05` tinyint(1) DEFAULT '0',
+  `selectedVeg06` tinyint(1) DEFAULT '0',
+  `selectedVeg07` tinyint(1) DEFAULT '0',
+  `selectedVeg08` tinyint(1) DEFAULT '0',
   `note` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`cid`)
+  PRIMARY KEY (`cid`),
+  CONSTRAINT `foreignkey_cid` FOREIGN KEY (`cid`) REFERENCES `customers` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -238,4 +251,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-23 18:25:13
+-- Dump completed on 2016-03-23 19:01:10
